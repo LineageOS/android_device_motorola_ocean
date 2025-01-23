0dc62a6c4c55bcf29128ccb824fc3e63d3f79f38
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-22 23:37:32 -0500

    ocean: Decommonize Health HAL
    Change-Id: I0420ff9dc1bf954a1e86ab0e42e3e6d3f1303651

--- device.mk
+++ device.mk
@@ -62,6 +62,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.ocean.rc
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sysfs
